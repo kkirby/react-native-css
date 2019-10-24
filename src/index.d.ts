0b8264250217d2fc2d538fad44c7fdf58050b9fb
Kyle Kirbatski <kkirbatski@gmrmarketing.com>
2019-10-24 16:11:10 -0500

    Add typescript stuff

---
+++ src/index.d.ts
@@ -0,0 +1,59 @@
+import * as React from 'react';
+
+type FunctionComponent<P> = React.FunctionComponent<
+	P & {
+		id?: Null<string>,
+		className?: Null<string>,
+		parentStyleInfo?: StyleInfo
+	}>;
+
+export function styleComponent<P>(
+	component: React.ComponentType<P>,
+	processChildren?: boolean,
+): FunctionComponent<P>;
+
+export function decorateElementForStyles<P>(
+	component: React.ComponentType,
+	processChildren?: boolean,
+	elementInheritsStyle?: boolean,
+): FunctionComponent<P>;
+
+export interface StyleInfo {
+	name: string;
+	selector: Selector;
+	parent: StyleInfo;
+	children: StyleInfo[];
+}
+
+interface Selector {
+	id?: string | null;
+	className?: string | null;
+}
+
+interface StyleAndProps {
+	style: {
+		[key: string]: string | number;
+	};
+	props: {
+		[key: string]: any;
+	};
+}
+
+export function useStyle(
+	name: string,
+	selector: Selector,
+	parent: StyleInfo,
+): {
+	styleInfo: StyleInfo;
+	styleAndProps: StyleAndProps;
+};
+
+interface RuleSets {
+	[key: string]: any;
+}
+
+export function pushRuleSets(ruleSets: RuleSets): void;
+
+export function importScss(scss: string): void;
+
+export function updateStyles(ruleSets: RuleSets): void;
